28249b6361ac7326e94ef80428941b0c530c855d
bpotatog <basketscott81223@gmail.com>
2014-08-23 22:26:01 +0800

    add another elevator in the building

--- examples/week2/Elevator/elevator.ino
+++ examples/week2/Elevator/elevator.ino
@@ -1,22 +1,28 @@
-int p1 = 2;//orange
-int p2 = 3;//yellow
-int p3 = 4;//pink
-int p4 = 5;//blue
-int btn3 = 10;//floor2
-int btn2 = 9;//floor1
-int btn1 = 8;//ground
+int p1 = 2;//orange wire
+int p2 = 3;//yellow wire
+int p3 = 4;//pink wire
+int p4 = 5;//blue wire
+/*********************************/
+int second_p1 = 9;//second motor orange wire
+int second_p2 = 10;//second motor yellow wire
+int second_p3 = 11;//second motor pink wire
+int second_p4 = 12;//second motor blue wire
+/*********************************/
+int btn3 = 8;// 2F
+int btn2 = 7;// 1F
+int btn1 = 6;// ground
 ///////////////////////////////////
-int now_position = 1;
-int now_position_dc = 5;
-int ground = 0;
-int floor1 = 0;
-int floor2 = 0;
+int now_position = 1;//first elevator stop layer
+int now_position_second = 1;// second elevator stop layer
+int ground = 0;// 2F flag
+int floor1 = 0;// 1F flag
+int floor2 = 0;// ground flag
 //////////////////////////////
-int inside_1 = 12;
-int inside_2 = 13;
-int led1 = 7;
-int led2 = 6;
-int led3 = 11;
+//int inside_1 = 12;
+//int inside_2 = 13;
+//int led1 = 7;
+//int led2 = 6;
+//int led3 = 11;
 void setup(){
   pinMode(p1, OUTPUT);
   pinMode(p2, OUTPUT);
@@ -25,42 +31,52 @@ void setup(){
   pinMode(btn3,INPUT_PULLUP);
   pinMode(btn2,INPUT_PULLUP);
   pinMode(btn1,INPUT_PULLUP);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
+  pinMode(second_p1, OUTPUT);
+  pinMode(second_p2, OUTPUT);
+  pinMode(second_p3, OUTPUT);
+  pinMode(second_p4, OUTPUT);
   Serial.begin(9600);
 }
-unsigned int t;
+unsigned long int t;// time(millis)
 void loop(){ 
-  //test();
-  if(digitalRead(btn1) == LOW && now_position != 1 && now_position_dc != 1){
-    ground = 1;Serial.println("1");
-    digitalWrite(led1, HIGH);
+  int ran_num = random(0, 2);
+  //Serial.println(ran_num);
+  if(digitalRead(btn1) == LOW && now_position != 1 && now_position_second != 1){
+    ground = 1;Serial.println("go to 1");
+    //digitalWrite(led1, HIGH);
   }
-  if(digitalRead(btn2) == LOW && now_position != 2 && now_position_dc != 2){
-    floor1 = 1;Serial.println("2");
-    digitalWrite(led2, HIGH);
+  if(digitalRead(btn2) == LOW && now_position != 2 && now_position_second != 2){
+    floor1 = 1;Serial.println("go to 2");
+    //digitalWrite(led2, HIGH);
   }
-  if(digitalRead(btn3) == LOW && now_position != 3 && now_position_dc != 3){
-    floor2 = 1;Serial.println("3");
-    digitalWrite(led3, HIGH);
+  if(digitalRead(btn3) == LOW && now_position != 3 && now_position_second != 3){
+    floor2 = 1;Serial.println("go to 3");
+    //digitalWrite(led3, HIGH);
   }
   if(ground == 1){
     ground = 0;
-    int dif1 = now_position - 1;
-    int dif2 = now_position_dc -1;
-    if(dif1 <= dif2){
+    int dif1 = abs(now_position - 1);
+    int dif2 = abs(now_position_second -1);
+    if(dif1 < dif2){
       down(1);
     }
-    else{
+    else if(dif1 == dif2){// random choose elevator
+      int num_random = random(0, 2);
+      if(num_random == 0)
+        down(1);
+      else
+        down_second(1);
     }
-    digitalWrite(led1, LOW);
+    else{
+      down_second(1);
+    }
+    //digitalWrite(led1, LOW);
   }
   else if(floor1 == 1){
     floor1 = 0;
     int dif1 = abs(now_position - 2);
-    int dif2 = abs(now_position_dc - 2);
-    if(dif1 <= dif2){
+    int dif2 = abs(now_position_second - 2);
+    if(dif1 < dif2){
       if(now_position > 2){
         down(2);
       }
@@ -68,213 +84,250 @@ void loop(){
         up(2);
       }
     }
-    else{
+    else if(dif1 == dif2){// random choose elevator
+      int num_random = random(0, 2);
+      if(num_random == 0){
+        if(now_position > 2)
+          down(2);
+        else
+          up(2);
+      }
+      else{
+        if(now_position_second > 2)
+          down_second(2);
+        else
+          up_second(2);
+      }
     }
-    digitalWrite(led2, LOW);
+    else{
+      if(now_position_second > 2){
+        down_second(2);
+      }
+      else{
+        up_second(2);
+      }
+    }
+    //digitalWrite(led2, LOW);
   }
   else if(floor2 == 1){
     floor2 = 0;
-    int dif1 = 3 - now_position;
-    int dif2 = 3 - now_position_dc;
-    if(dif1 <= dif2){
+    int dif1 = abs(3 - now_position);
+    int dif2 = abs(3 - now_position_second);
+    if(dif1 < dif2){
       up(3);
     }
-    else{
+    else if(dif1 == dif2){// random choose elevator
+      int num_random = random(0, 2);
+      if(num_random == 0)
+        up(3);
+      else
+        up_second(3);
     }
-    digitalWrite(led3, LOW);
+    else{
+      up_second(3);
+    }
+    //digitalWrite(led3, LOW);
   }
 }
 void up(int n){
-  Serial.println("what's wrong");
+  Serial.println("First elevator is up");
   int need_to_move = n - now_position;
   t = millis();
-  Serial.println(need_to_move);
-  Serial.print(t);Serial.print(", ");Serial.print(millis());
+  //Serial.println(need_to_move);
+  Serial.print(t);Serial.print(", ");Serial.println(millis());
   while((millis() - t) < (10000 * need_to_move)){
     digitalWrite(p1, HIGH);
     digitalWrite(p2, LOW);
     digitalWrite(p3, LOW);
     digitalWrite(p4, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p1, HIGH);
     digitalWrite(p2, HIGH);
     digitalWrite(p3, LOW);
     digitalWrite(p4, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p1, LOW);
     digitalWrite(p2, HIGH);
     digitalWrite(p3, LOW);
     digitalWrite(p4, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p1, LOW);
     digitalWrite(p2, HIGH);
     digitalWrite(p3, HIGH);
     digitalWrite(p4, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p1, LOW);
     digitalWrite(p2, LOW);
     digitalWrite(p3, HIGH);
     digitalWrite(p4, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p1, LOW);
     digitalWrite(p2, LOW);
     digitalWrite(p3, HIGH);
     digitalWrite(p4, HIGH);
-    delay(2);
+    delay(3);
     digitalWrite(p1, LOW);
     digitalWrite(p2, LOW);
     digitalWrite(p3, LOW);
     digitalWrite(p4, HIGH);
-    delay(2);
+    delay(3);
     digitalWrite(p1, HIGH);
     digitalWrite(p2, LOW);
     digitalWrite(p3, LOW);
     digitalWrite(p4, HIGH);
-    delay(2);
+    delay(3);
   }
   now_position = n;
 }
 void down(int n){
   int need_to_move = now_position - n;
   t = millis();
-  Serial.println("down what");
-  Serial.println(need_to_move);
-  Serial.print(t);Serial.print(", ");Serial.print(millis());
+  Serial.println("First elevator is down");
+  //Serial.println(need_to_move);
+  Serial.print(t);Serial.print(", ");Serial.println(millis());
   while((millis() - t) < (10000 * need_to_move)){
     //Serial.println("cue");
     digitalWrite(p4, HIGH);
     digitalWrite(p3, LOW);
     digitalWrite(p2, LOW);
     digitalWrite(p1, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p4, HIGH);
     digitalWrite(p3, HIGH);
     digitalWrite(p2, LOW);
     digitalWrite(p1, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p4, LOW);
     digitalWrite(p3, HIGH);
     digitalWrite(p2, LOW);
     digitalWrite(p1, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p4, LOW);
     digitalWrite(p3, HIGH);
     digitalWrite(p2, HIGH);
     digitalWrite(p1, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p4, LOW);
     digitalWrite(p3, LOW);
     digitalWrite(p2, HIGH);
     digitalWrite(p1, LOW);
-    delay(2);
+    delay(3);
     digitalWrite(p4, LOW);
     digitalWrite(p3, LOW);
     digitalWrite(p2, HIGH);
     digitalWrite(p1, HIGH);
-    delay(2);
+    delay(3);
     digitalWrite(p4, LOW);
     digitalWrite(p3, LOW);
     digitalWrite(p2, LOW);
     digitalWrite(p1, HIGH);
-    delay(2);
+    delay(3);
     digitalWrite(p4, HIGH);
     digitalWrite(p3, LOW);
     digitalWrite(p2, LOW);
     digitalWrite(p1, HIGH);
-    delay(2);
+    delay(3);
   }
   now_position = n;
 }
-void test(){
+void up_second(int n){
+  Serial.println("Second elevator is up");
+  int need_to_move = n - now_position_second;
   t = millis();
   //Serial.println(need_to_move);
-  //Serial.print(t);Serial.print(", ");Serial.print(millis());
-  for(int i=0 ; i<99 ; i++){
-    if(i==98){
-      for(int j=0 ; j<100000 ; j++){
-        digitalWrite(p1, HIGH);
-        digitalWrite(p2, LOW);
-        digitalWrite(p3, LOW);
-        digitalWrite(p4, LOW);
-        delay(100-i);
-        digitalWrite(p1, HIGH);
-        digitalWrite(p2, HIGH);
-        digitalWrite(p3, LOW);
-        digitalWrite(p4, LOW);
-        delay(100-i);
-        digitalWrite(p1, LOW);
-        digitalWrite(p2, HIGH);
-        digitalWrite(p3, LOW);
-        digitalWrite(p4, LOW);
-        delay(100-i);
-        digitalWrite(p1, LOW);
-        digitalWrite(p2, HIGH);
-        digitalWrite(p3, HIGH);
-        digitalWrite(p4, LOW);
-        delay(100-i);
-        digitalWrite(p1, LOW);
-        digitalWrite(p2, LOW);
-        digitalWrite(p3, HIGH);
-        digitalWrite(p4, LOW);
-        delay(100-i);
-        digitalWrite(p1, LOW);
-        digitalWrite(p2, LOW);
-        digitalWrite(p3, HIGH);
-        digitalWrite(p4, HIGH);
-        delay(100-i);
-        digitalWrite(p1, LOW);
-        digitalWrite(p2, LOW);
-        digitalWrite(p3, LOW);
-        digitalWrite(p4, HIGH);
-        delay(100-i);
-        digitalWrite(p1, HIGH);
-        digitalWrite(p2, LOW);
-        digitalWrite(p3, LOW);
-        digitalWrite(p4, HIGH);
-        delay(100-i);
-      }
-    }
-    else{
-      digitalWrite(p1, HIGH);
-      digitalWrite(p2, LOW);
-      digitalWrite(p3, LOW);
-      digitalWrite(p4, LOW);
-      delay(100-i);
-      digitalWrite(p1, HIGH);
-      digitalWrite(p2, HIGH);
-      digitalWrite(p3, LOW);
-      digitalWrite(p4, LOW);
-      delay(100-i);
-      digitalWrite(p1, LOW);
-      digitalWrite(p2, HIGH);
-      digitalWrite(p3, LOW);
-      digitalWrite(p4, LOW);
-      delay(100-i);
-      digitalWrite(p1, LOW);
-      digitalWrite(p2, HIGH);
-      digitalWrite(p3, HIGH);
-      digitalWrite(p4, LOW);
-      delay(100-i);
-      digitalWrite(p1, LOW);
-      digitalWrite(p2, LOW);
-      digitalWrite(p3, HIGH);
-      digitalWrite(p4, LOW);
-      delay(100-i);
-      digitalWrite(p1, LOW);
-      digitalWrite(p2, LOW);
-      digitalWrite(p3, HIGH);
-      digitalWrite(p4, HIGH);
-      delay(100-i);
-      digitalWrite(p1, LOW);
-      digitalWrite(p2, LOW);
-      digitalWrite(p3, LOW);
-      digitalWrite(p4, HIGH);
-      delay(100-i);
-      digitalWrite(p1, HIGH);
-      digitalWrite(p2, LOW);
-      digitalWrite(p3, LOW);
-      digitalWrite(p4, HIGH);
-      delay(100-i);
-    }
+  Serial.print(t);Serial.print(", ");Serial.println(millis());
+  while((millis() - t) < (10000 * need_to_move)){
+    digitalWrite(second_p1, HIGH);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p4, LOW);
+    delay(3);
+    digitalWrite(second_p1, HIGH);
+    digitalWrite(second_p2, HIGH);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p4, LOW);
+    delay(3);
+    digitalWrite(second_p1, LOW);
+    digitalWrite(second_p2, HIGH);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p4, LOW);
+    delay(3);
+    digitalWrite(second_p1, LOW);
+    digitalWrite(second_p2, HIGH);
+    digitalWrite(second_p3, HIGH);
+    digitalWrite(second_p4, LOW);
+    delay(3);
+    digitalWrite(second_p1, LOW);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p3, HIGH);
+    digitalWrite(second_p4, LOW);
+    delay(3);
+    digitalWrite(second_p1, LOW);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p3, HIGH);
+    digitalWrite(second_p4, HIGH);
+    delay(3);
+    digitalWrite(second_p1, LOW);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p4, HIGH);
+    delay(3);
+    digitalWrite(second_p1, HIGH);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p4, HIGH);
+    delay(3);
   }
+  now_position_second = n;
+}
+void down_second(int n){
+  int need_to_move = now_position_second - n;
+  t = millis();
+  Serial.println("Second elevator is down");
+  //Serial.println(need_to_move);
+  Serial.print(t);Serial.print(", ");Serial.println(millis());
+  while((millis() - t) < (10000 * need_to_move)){
+    //Serial.println("cue");
+    digitalWrite(second_p4, HIGH);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p1, LOW);
+    delay(3);
+    digitalWrite(second_p4, HIGH);
+    digitalWrite(second_p3, HIGH);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p1, LOW);
+    delay(3);
+    digitalWrite(second_p4, LOW);
+    digitalWrite(second_p3, HIGH);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p1, LOW);
+    delay(3);
+    digitalWrite(second_p4, LOW);
+    digitalWrite(second_p3, HIGH);
+    digitalWrite(second_p2, HIGH);
+    digitalWrite(second_p1, LOW);
+    delay(3);
+    digitalWrite(second_p4, LOW);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p2, HIGH);
+    digitalWrite(second_p1, LOW);
+    delay(3);
+    digitalWrite(second_p4, LOW);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p2, HIGH);
+    digitalWrite(second_p1, HIGH);
+    delay(3);
+    digitalWrite(second_p4, LOW);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p1, HIGH);
+    delay(3);
+    digitalWrite(second_p4, HIGH);
+    digitalWrite(second_p3, LOW);
+    digitalWrite(second_p2, LOW);
+    digitalWrite(second_p1, HIGH);
+    delay(3);
+  }
+  now_position_second = n;
 }
